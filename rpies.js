4c11c63a27d3a24fe4682437c841e3e444caf233
Ezra Dowd <ezra.dowd@gmail.com>
2016-04-21 13:45:17 -0400

    Initial commit

---
+++ rpies.js
@@ -0,0 +1,23 @@
+function wipePage(){
+    $("body").html("");
+}
+
+function pageInject(injectPoint, injectResource){
+    $.get(chrome.extension.getURL(injectResource),function(resource){
+        $(injectPoint).append(resource);
+    });
+}
+
+$(document).ready(function(){;
+    switch (document.title){
+        case "Rensselaer's Student Information System":
+            wipePage();
+            pageInject("body", "rpiesTitle.html");
+            break;
+        case "User Login":
+            alert("Injected onto login page");
+            break
+        default:
+            alert("Injected onto non-handled page");
+    }
+});
