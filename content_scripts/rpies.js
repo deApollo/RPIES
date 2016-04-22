c52227f5d5cf5b38f8825b704a2d02f89ce8d150
Ezra Dowd <ezra.dowd@gmail.com>
2016-04-22 09:29:20 -0400

    Put project into a better structure

--- content_scripts/rpies.js
+++ content_scripts/rpies.js
@@ -12,7 +12,7 @@ $(document).ready(function(){;
     switch (document.title){
         case "Rensselaer's Student Information System":
             wipePage();
-            pageInject("body", "rpiesTitle.html");
+            pageInject("body", "web_res/rpiesTitle.html");
             break;
         case "User Login":
             alert("Injected onto login page");
