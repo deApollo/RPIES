d82f84b5e15e684a3285d746fbc7720b4b0939ab
Ezra Dowd <ezra.dowd@gmail.com>
2016-04-24 15:22:26 -0400

    Added some more pages

--- content_scripts/rpies.js
+++ content_scripts/rpies.js
@@ -1,26 +1,36 @@
-function wipePage(point){
+function wipePage(point) {
     $(point).html("");
 }
 
-function pageInject(injectPoint, injectResource){
-    $.get(chrome.extension.getURL(injectResource),function(resource){
+function pageInject(injectPoint, injectResource) {
+    $.get(chrome.extension.getURL(injectResource), function(resource) {
         $(injectPoint).append(resource);
     });
 }
 
-$(document).ready(function(){;
-    switch (document.title){
-        case "Rensselaer's Student Information System":
-            wipePage("body");
-            pageInject("body", "web_res/rpiesTitle.html");
-            break;
-        case "User Login":
-            alert("Injected onto login page");
-            break
-        case "Change User Pin":
-            wipePage();
-            pageInject("body")
-        default:
-            alert("Injected onto non-handled page");
+$(document).ready(function() {
+    switch (document.title) {
+    case "Rensselaer's Student Information System":
+        wipePage("body");
+        pageInject("body", "web_res/rpiesTitle.html");
+        break;
+    case "Main Menu":
+        wipePage("body");
+        pageInject("body", "web_res/rpiesMainMenu.html");
+        break;
+    case "Personal Information Menu":
+        wipePage("body");
+        pageInject("body", "web_res/rpiesPersonalInfoMenu.html");
+        break;
+    case "Change User PIN":
+        wipePage("body");
+        pageInject("body", "web_res/rpiesChangeUserPin.html");
+        break;
+    case "Student Menu":
+        wipePage("body");
+        pageInject("body", "web_res/rpiesStudentMenu.html");
+        break;
+    default:
+        break;
     }
 });
