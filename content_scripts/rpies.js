564ca41fe17438750d892b686b377f3ca81c0860
Conrad Mossl <conradmossl@gmail.com>
2016-05-09 13:28:10 -0400

    Conrad made changes!

--- content_scripts/rpies.js
+++ content_scripts/rpies.js
@@ -9,6 +9,7 @@ function pageInject(injectPoint, injectResource) {
 }
 
 $(document).ready(function() {
+    console.log()
     switch (document.title) {
     case "Rensselaer's Student Information System":
         wipePage("body");
@@ -16,29 +17,66 @@ $(document).ready(function() {
         break;
     case "Main Menu":
         wipePage("body");
+        pageInject("body", "web_res/navBar.html");
         pageInject("body", "web_res/rpiesMainMenu.html");
         break;
-    case "Personal Information Menu":
+    case "User Login":
         wipePage("body");
-        pageInject("body", "web_res/rpiesPersonalInfoMenu.html");
+        pageInject("body", "web_res/rpiesTitle.html");
+        break;
+    case "Personal Information Menu":
+        if ($(".informationtext").text() == "Your changes were saved successfully") {
+            wipePage("body");
+            pageInject("body", "web_res/navBar.html");
+            pageInject("body", "web_res/rpiesPersonalInfoMenuConfirm.html");
+        }
+        else {
+            wipePage("body");
+            pageInject("body", "web_res/navBar.html");
+            pageInject("body", "web_res/rpiesPersonalInfoMenu.html");
+        }
         break;
     case "Change User PIN":
-        wipePage("body");
-        pageInject("body", "web_res/rpiesChangeUserPin.html");
+        if (($(".pldefault:last").text().trim()) == "New PIN number verification error!") {
+            wipePage("body");
+            pageInject("body", "web_res/navBar.html");
+            pageInject("body", "web_res/rpiesChangeUserPinError.html");
+        }
+        else {
+            wipePage("body");
+            pageInject("body", "web_res/navBar.html");
+            pageInject("body", "web_res/rpiesChangeUserPin.html");
+        }
         break;
     case "Student Menu":
         wipePage("body");
+        pageInject("body", "web_res/navBar.html");
         pageInject("body", "web_res/rpiesStudentMenu.html");
         break;
     case "Financial Aid":
         wipePage("body");
+        pageInject("body", "web_res/navBar.html");
         pageInject("body", "web_res/rpiesFinancialAidMenu.html");
+        break;
+    case "Search Result":
+        wipePage("body");
+        pageInject("body", "web_res/navBar.html");
+        pageInject("body", "web_res/rpiesSearchResults.html");
         break;
     case "User Logout":
         wipePage("body");
         pageInject("body", "web_res/rpiesLogout.html");
         break;
     default:
-        break;
+        console.log(window.location.href);
+        switch (window.location.href) {
+            case "https://sis.rpi.edu/rss/twbkwbis.P_GenMenu?name=pmenu.P_MainMnu":
+                wipePage("body");
+                pageInject("body", "web_res/navBar.html");
+                pageInject("body", "web_res/rpiesHumanResourcesPayrollMenu.html")
+                break;
+            default:
+                break;
+        }
     }
 });
